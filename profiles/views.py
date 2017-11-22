236a7b684b7a9f6a25c5954ee73f7a41857bbe58
harut <harutyun.ghubasaryan@betconstruct.com>
2017-11-22 15:51:33 +0400

    profile page added, login_required decorator

--- profiles/views.py
+++ profiles/views.py
@@ -1,5 +1,5 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
-
 
 # Create your views here.
 def home(request):
@@ -7,8 +7,14 @@ def home(request):
     template = 'home.html'
     return render(request, template, context)
 
-
 def about(request):
     context = {}
     template = 'about.html'
+    return render(request, template, context)
+
+@login_required
+def userProfile(request):
+    user = request.user
+    context = {'user': user}
+    template = 'profile.html'
     return render(request, template, context)
